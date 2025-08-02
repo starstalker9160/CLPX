3e87e533b16006fff7c31972d24170cca9a5e830
starstalker9160 <abyazjschool@gmail.com>
2025-08-02 11:30:09 +0400

    started working on macos client

---
+++ CLPX-client-MacOS/CLPX-client-MacOSTests/CLPX_client_MacOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CLPX_client_MacOSTests.swift
+//  CLPX-client-MacOSTests
+//
+//  Created by star.stalker9160 on 02/08/2025.
+//
+
+import Testing
+@testable import CLPX_client_MacOS
+
+struct CLPX_client_MacOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
